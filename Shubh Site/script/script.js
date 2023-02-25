1f3bf132dae5a824c8e459cf673b58781cddce5f
Vizan <101347615+xVizan@users.noreply.github.com>
2023-02-25 18:48:48 +0530

    Add files via upload

---
+++ Shubh Site/script/script.js
@@ -0,0 +1,61 @@
+// $(function(){
+//     var str = '#len'; //increment by 1 up to 1-nelemnts
+//     $(document).ready(function(){
+//       var i, stop;
+//       i = 1;
+//       stop = 4; //num elements
+//       setInterval(function(){
+//         if (i > stop){
+//           return;
+//         }
+//         $('#len'+(i++)).toggleClass('bounce');
+//       }, 500)
+//     });
+//   });
+
+(function($) { "use strict";
+
+	$(function() {
+		var header = $(".start-style");
+		$(window).scroll(function() {    
+			var scroll = $(window).scrollTop();
+		
+			if (scroll >= 10) {
+				header.removeClass('start-style').addClass("scroll-on");
+			} else {
+				header.removeClass("scroll-on").addClass('start-style');
+			}
+		});
+	});		
+		
+	//Animation
+	
+	$(document).ready(function() {
+		$('body.hero-anime').removeClass('hero-anime');
+	});
+
+	//Menu On Hover
+		
+	$('body').on('mouseenter mouseleave','.nav-item',function(e){
+			if ($(window).width() > 750) {
+				var _d=$(e.target).closest('.nav-item');_d.addClass('show');
+				setTimeout(function(){
+				_d[_d.is(':hover')?'addClass':'removeClass']('show');
+				},1);
+			}
+	});	
+	
+	//Switch light/dark
+	
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("dark")) {
+			$("body").removeClass("dark");
+			$("#switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("dark");
+			$("#switch").addClass("switched");
+		}
+	});  
+	
+  })(jQuery);
